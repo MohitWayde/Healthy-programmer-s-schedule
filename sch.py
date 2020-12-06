4f92fed4eed703d06684466fffa7bd52d36f1038
MohitWayde <62068704+MohitWayde@users.noreply.github.com>
2020-12-06 15:51:06 +0530

    timestamp, time updated(minor changes)

--- sch.py
+++ sch.py
@@ -3,16 +3,16 @@ import datetime
 import time
 from pygame import mixer
 # Global declaration
-water = 3480 # 58 mins
-eyes = 1020 # 17 mins  
-exercise = 2700 # 45mins
+water = 40*60 #40 mins 40*60
+eyes = 20*60 #20 mins 20*60
+exercise = 45*60 # 45 mins 45*60
 # Functions
 def musicforsch(file, stopper_msg): 
     mixer.init()
     mixer.music.load(file)
     mixer.music.play()
     while True:
-        stop_music_text = input("Enter stopper message :")
+        stop_music_text = input()
         if stop_music_text == stopper_msg:
             mixer.music.stop()
             break
@@ -29,21 +29,24 @@ except Exception as e:
 
 def scheduler_app():
     
-        print("schedule started")
+        print("schedule started at ",datetime.datetime.now())
         init_water = time.time()
         init_eyes = time.time()
         init_exercise = time.time()
 
         while True:
             if  time.time()-init_water > water:
+                print("DON'T SKIP! Water time .............Type wdone to stop the song")
                 musicforsch("water.mp3", "wdone") # dont forget to change filename, paste any mp3 file in the folder where this python file is located
                 logfile("Drank water at      :")
                 init_water = time.time()
             if  time.time()-init_eyes > eyes:
+                print("DON'T SKIP! Eyes exercise time......Type edone to stop the song")
                 musicforsch("eyes.mp3", "edone") # dont forget to change filename, paste any mp3 file in the folder where this python file is located
                 logfile("Eyes exercise at    :")
                 init_eyes = time.time()
             if  time.time()-init_exercise > exercise:
+                print("DON'T SKIP! physical exercise time..Type pdone to stop the song")
                 musicforsch("hpysicalact.mp3", "pdone") # dont forget to change filename, paste any mp3 file in the folder where this python file is located
                 logfile("PhMovement at       :") 
                 init_exercise = time.time()
